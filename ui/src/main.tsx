68b3bb44c7d7a85b7a31717faf56669c1abf91b3
jett59 <jett.thompson.59@gmail.com>
2024-12-10 19:37:47 +1100

    add the google sign in button

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { HashRouter } from 'react-router-dom';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <GoogleOAuthProvider clientId='440807713733-li6c8t0f1pmjrlaceen6afndskghlrrm.apps.googleusercontent.com'>
+    <StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </StrictMode>
+  </GoogleOAuthProvider>,
 )
