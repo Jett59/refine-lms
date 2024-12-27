20b305eb742b72798a507bc1ecc62de65c262314
jett59 <jett.thompson.59@gmail.com>
2024-12-28 09:32:12 +1100

    add code to create a document with the user's information on api call

--- api/handler-authenticated.ts
+++ api/handler-authenticated.ts
@@ -1,14 +1,20 @@
 import { APIGatewayProxyEventV2WithJWTAuthorizer, APIGatewayProxyStructuredResultV2, Context } from "aws-lambda";
 import { errorResponse, getPath, raiseInternalServerError } from "./handlers";
+import { MongoClient } from "mongodb";
+import { createUser, findUser, findUserByJwtUserId, User } from "./user";
 
-const DATABASE_NAME = `${process.env.REFINE_LMS_DATABASE ?? 'refine-dev'}`
+const DATABASE_NAME = process.env.REFINE_LMS_DATABASE ?? 'refine-dev'
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING ?? 'mongodb://127.0.0.1:27017'
+
+const mongoClient: MongoClient = new MongoClient(CONNECTION_STRING)
 
 exports.handler = async (event: APIGatewayProxyEventV2WithJWTAuthorizer, context: Context): Promise<APIGatewayProxyStructuredResultV2> => {
     try {
         // Set this flag on the context as specified in MongoDB best practices for caching in Lambda:
         // https://docs.atlas.mongodb.com/best-practices-connecting-to-aws-lambda/
         (context as any).callbackWaitsForEmptyEventLoop = false
+
+        const db = await mongoClient.db(DATABASE_NAME)
 
         const path = getPath(event)
         let body
@@ -18,9 +24,28 @@ exports.handler = async (event: APIGatewayProxyEventV2WithJWTAuthorizer, context
             } catch (e) { }
         }
 
-        const userId = event.requestContext.authorizer.jwt.claims.sub
-        if (!userId) {
-            return errorResponse(401, 'Missing user ID')
+        const jwtUserId = event.requestContext.authorizer.jwt.claims.sub
+        if (!jwtUserId) {
+            return errorResponse(401, 'Missing user ID in JWT')
+        }
+        if (!(typeof jwtUserId === 'string')) {
+            return errorResponse(401, 'Invalid user ID in JWT')
+        }
+        let user = await findUserByJwtUserId(db, jwtUserId)
+        if (!user) {
+            const jwtClaims = event.requestContext.authorizer.jwt.claims
+            if (jwtClaims.name && jwtClaims.email && jwtClaims.picture) {
+                const newUser: User = {
+                    jwtUserId,
+                    name: jwtClaims.name.toString(),
+                    email: jwtClaims.email.toString(),
+                    picture: jwtClaims.picture.toString(),
+                }
+                const userId = await createUser(db, newUser)
+                user = await findUser(db, userId)
+            }else {
+                return errorResponse(401, 'Missing user information in JWT')
+            }
         }
 
         switch (path) {
---
+++ api/user.ts
@@ -0,0 +1,30 @@
+import { Db, ObjectId } from "mongodb";
+
+export interface User {
+    _id?: ObjectId
+    jwtUserId: string
+    name: string
+    email: string
+    picture: string
+}
+
+const COLLECTION_NAME = 'users'
+
+function getCollection(db: Db) {
+    return db.collection<User>(COLLECTION_NAME)
+}
+
+export async function findUser(db: Db, userId: ObjectId): Promise<User | null> {
+    return await getCollection(db).findOne({
+        _id: userId
+    })
+}
+
+export async function findUserByJwtUserId(db: Db, jwtUserId: string): Promise<User | null> {
+    return await getCollection(db).findOne({ jwtUserId })
+}
+
+export async function createUser(db: Db, user: User): Promise<ObjectId> {
+    const result = await getCollection(db).insertOne(user)
+    return result.insertedId
+}
