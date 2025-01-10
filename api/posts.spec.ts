d565cf56ebae67bdc82190579d1929ec1b46a795
jett59 <jett.thompson.59@gmail.com>
2025-01-11 09:39:17 +1100

    add a new data structure for a post with a function to create one

---
+++ api/posts.spec.ts
@@ -0,0 +1,104 @@
+import { Collection, Db, MongoClient, ObjectId } from "mongodb"
+import { createPost, Post } from "./posts"
+import { createUser } from "./user"
+import { School } from "./schools"
+
+function createSchoolStructure(schoolId: ObjectId, schoolStudentIds: ObjectId[], yearGroupId: ObjectId, courseId: ObjectId, classId: ObjectId, classStudentIds: ObjectId[]): School {
+    return {
+        _id: schoolId,
+        name: 'School 1',
+        yearGroups: [{
+            id: yearGroupId,
+            name: 'Year 1',
+            courses: [{
+                id: courseId,
+                name: 'Course 1',
+                classes: [{
+                    id: classId,
+                    name: 'Class 1',
+                    teacherIds: [],
+                    studentIds: classStudentIds,
+                    requestingStudentIds: []
+                }]
+            }]
+        }],
+        administratorIds: [],
+        teacherIds: [],
+        studentIds: schoolStudentIds,
+        invitedAdministratorEmails: [],
+        invitedTeacherEmails: [],
+        invitedStudentEmails: []
+    }
+}
+
+describe("Posts", () => {
+    const mongoClient: MongoClient = new MongoClient('mongodb://localhost:27017')
+    let db: Db
+    let postsCollection: Collection<Post>
+
+    let user1: ObjectId
+    let user2: ObjectId
+    let user3: ObjectId
+
+    const schoolId = new ObjectId()
+    const yearGroupId = new ObjectId()
+    const courseId = new ObjectId()
+    const classId = new ObjectId()
+
+    beforeAll(async () => {
+        await mongoClient.connect()
+        db = mongoClient.db('test')
+        postsCollection = db.collection('posts')
+        await postsCollection.drop()
+    })
+    afterAll(async () => {
+        await mongoClient.close()
+    })
+
+    beforeEach(async () => {
+        user1 = await createUser(db, { name: 'User 1', jwtUserId: 'abc123', email: 'user1', picture: '' })
+        user2 = await createUser(db, { name: 'User 2', jwtUserId: 'bcd234', email: 'user2', picture: '' })
+        user3 = await createUser(db, { name: 'User 3', jwtUserId: 'cde345', email: 'user3', picture: '' })
+    })
+
+    it("Should create a post", async () => {
+        const school: School = createSchoolStructure(schoolId, [], yearGroupId, courseId, classId, [])
+
+        const post: Post = {
+            posterId: user1,
+            schoolId: schoolId,
+            yearGroupId: yearGroupId,
+            courseId: courseId,
+            classIds: [classId],
+            private: false,
+            type: 'post',
+            title: 'Hello',
+            content: 'Hello World',
+            attachments: []
+        }
+
+        const postId = await createPost(db, school, post)
+
+        const postFromDatabase = await postsCollection.findOne({ _id: postId })
+        expect(postFromDatabase).toEqual({ _id: postId, ...post })
+    })
+    it("Should filter the class list for students", async () => {
+        const school: School = createSchoolStructure(schoolId, [user1], yearGroupId, courseId, classId, [user1])
+        const post: Post = {
+            posterId: user1,
+            schoolId: schoolId,
+            yearGroupId: yearGroupId,
+            courseId: courseId,
+            classIds: [classId, new ObjectId()],
+            private: false,
+            type: 'post',
+            title: 'Hello',
+            content: 'Hello World',
+            attachments: []
+        }
+
+        const postId = await createPost(db, school, post)
+        const postFromDatabase = await postsCollection.findOne({ _id: postId })
+        expect(postFromDatabase).toEqual({ _id: postId, ...post, classIds: [classId] })
+    })
+})
---
+++ api/posts.ts
@@ -0,0 +1,50 @@
+import { Db, ObjectId } from "mongodb"
+import { Course, School } from "./schools"
+
+export interface Post {
+    _id?: ObjectId
+    posterId: ObjectId
+    schoolId: ObjectId
+    yearGroupId: ObjectId
+    courseId?: ObjectId
+    classIds?: ObjectId[]
+    private: boolean
+    type: 'post' | 'material' | 'assignment'
+    title: string
+    content: string
+    attachments: Attachment[]
+}
+
+export interface Attachment {
+    id: ObjectId
+    title: string
+    link: string
+}
+
+const COLLECTION_NAME = 'posts'
+
+function getCollection(db: Db) {
+    return db.collection<Post>(COLLECTION_NAME)
+}
+
+function filterClassList(posterId: ObjectId, school: School, course: Course, classIds: ObjectId[]) {
+    // The classes should only be the ones which are visible to the poster
+    // Non-students can see everything
+    if (school.studentIds.some(student => student.equals(posterId))) {
+        return classIds.filter(classId => course.classes.find(cls => cls.id.equals(classId))?.studentIds.some(studentId => studentId.equals(posterId)))
+    }else {
+        return classIds
+    }
+}
+
+export async function createPost(db: Db, school: School, post: Post) {
+    const postCopy = { ...post }
+    if (post.courseId && post.classIds) {
+        const course = school.yearGroups.find(yg => yg.id.equals(post.yearGroupId))?.courses.find(c => c.id.equals(post.courseId))
+        if (course) {
+            postCopy.classIds = filterClassList(post.posterId, school, course, post.classIds)
+        }
+    }
+    const response = await getCollection(db).insertOne(postCopy)
+    return response.insertedId
+}
